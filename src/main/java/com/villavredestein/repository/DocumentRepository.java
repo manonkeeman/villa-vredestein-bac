8603223bc687bef3244373877666e1d972bce1c6
manonkeeman <manonkeeman@gmail.com>
2026-01-09 16:12:00 +0100

    Add DocumentResponseDTO and secure document endpoints

--- src/main/java/com/villavredestein/repository/DocumentRepository.java
+++ src/main/java/com/villavredestein/repository/DocumentRepository.java
@@ -1,7 +1,22 @@
 package com.villavredestein.repository;
 
 import com.villavredestein.model.Document;
+import com.villavredestein.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+
+    List<Document> findAllByOrderByIdDesc();
+
+    List<Document> findByRoleAccessOrderByIdDesc(String roleAccess);
+
+    List<Document> findByUploadedByOrderByIdDesc(User uploadedBy);
+
+    List<Document> findByRoleAccessAndUploadedByOrderByIdDesc(String roleAccess, User uploadedBy);
+    Optional<Document> findByStoragePath(String storagePath);
 }

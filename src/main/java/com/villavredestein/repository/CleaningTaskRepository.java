0649c529e9bd1aae851cdc212667d9e5227dff03
manonkeeman <manonkeeman@gmail.com>
2026-01-09 15:17:58 +0100

    Add CleaningResponseDTO and refactor cleaning endpoints

--- src/main/java/com/villavredestein/repository/CleaningTaskRepository.java
+++ src/main/java/com/villavredestein/repository/CleaningTaskRepository.java
@@ -3,9 +3,11 @@ package com.villavredestein.repository;
 import com.villavredestein.model.CleaningTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public interface CleaningTaskRepository extends JpaRepository<CleaningTask, Long> {
+
     List<CleaningTask> findByWeekNumberOrderByIdAsc(int weekNumber);
 }
